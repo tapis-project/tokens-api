93ea252ce2d0c7042e97a6a53ae04fb9a9c5ae11
Joe Stubbs <jstubbs@tacc.utexas.edu>
2019-11-15 15:26:19 -0600

    add basic authn/z framework to enable checking the http method and URL.

--- service/api.py
+++ service/api.py
@@ -1,9 +1,14 @@
 from common.utils import TapisApi, handle_error, flask_errors_dict
 
+from service.auth import authn_and_authz
 from service.controllers import TokensResource
 
 from service import app, db
 
+# authentication and authorization ---
+@app.before_request
+def authnz_for_authenticator():
+    authn_and_authz()
 
 # flask restful API object ----
 api = TapisApi(app, errors=flask_errors_dict)
