3dcbe1d393bf8a0fb0cf6fbfe1f16109fe9fba37
Joe Stubbs <jstubbs@tacc.utexas.edu>
2020-02-04 14:23:17 -0600

    add master to list of tenants, and generate a service token for itself.

--- service/auth.py
+++ service/auth.py
@@ -1,12 +1,27 @@
+from common.auth import get_service_tapy_client
 from common.config import conf
 from common import errors as common_errors
 from flask import g, request
 from service.errors import InvalidTokenClaimsError
-from service.models import TapisAccessToken
+from service.models import AccessTokenData, TapisAccessToken
+from service import get_tenant_config
 
 # get the logger instance -
 from common.logs import get_logger
 logger = get_logger(__name__)
+
+# 10 years TTL
+SERVICE_TOKEN_TTL = 60*60*24*365*10
+
+# this is the Tapis client that tokens will use for interacting with other services, such as the security kernel.
+token_tenant = get_tenant_config(tenant_id=conf.service_tenant_id)
+d = AccessTokenData(token_tenant_id=conf.service_tenant_id, token_username=conf.service_name, account_type = 'service')
+d.access_token_ttl =  SERVICE_TOKEN_TTL
+token_data = TapisAccessToken.get_derived_values(d)
+jwt = TapisAccessToken(**token_data)
+jwt.sign_token()
+t = get_service_tapy_client(jwt=jwt)
+
 
 def authn_and_authz():
     """
@@ -26,6 +41,11 @@ def authn_and_authz():
         logger.info("Request has no url_rule")
         raise common_errors.BaseTapisError(
             "Invalid request: the API endpoint does not exist or the provided HTTP method is not allowed.", 405)
+    # if we are using the SK, we require basic auth on generating tokens (access or refresh).
+    if conf.use_sk:
+        if request.method == 'POST' or request.method == 'PUT':
+            pass
+            # do basic auth with SK and tapis client, t...
 
 
 def check_extra_claims(extra_claims):
